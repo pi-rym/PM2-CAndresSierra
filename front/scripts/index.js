eaeaf5f7ddc2be58f517477b4c40d6b9cf8960c6
CAndresSierra <camilofullstack@gmail.com>
2024-03-05 17:28:46 -0500

    homework2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,50 +1,32 @@
-tempData.map((item) => {
-    const target = document.createElement('div');
 
-    const titulo = document.createElement('a')
-    titulo.textContent = item.title;
-    titulo.setAttribute('href', '#')
-
-    const img = document.createElement('img')
-    img.src = item.poster;
-
-    const año = document.createElement('p')
-    año.textContent = item.year;
-
-    const director = document.createElement('p')
-    director.textContent = item.director;
-
-    const duracion = document.createElement('p')
-    duracion.textContent = item.duration;
-
-    const genero = document.createElement('p')
-    genero.textContent = item.genre;
-
-    const calificacion = document.createElement('p')
-    calificacion.textContent = item.rate;
-
-    target.classList.add('target');
-    titulo.classList.add('titulo');
-    img.classList.add('poster');
-    año.classList.add('año');
-    director.classList.add('director');
-    duracion.classList.add('duracion');
-    genero.classList.add('genero');
-    calificacion.classList.add('calificacion');
-
-    target.appendChild(titulo);
-    target.appendChild(img);
-    target.appendChild(año);
-    target.appendChild(director);
-    target.appendChild(duracion);
-    target.appendChild(genero);
-    target.appendChild(calificacion);
-    
-    const containerCards = document.getElementById('container-cards')
-    containerCards.appendChild(target);
-    
+$.get('https://students-api.2.us-1.fl0.io/movies', (data) => {
+    renderCard(data);
 })
 
+const renderCard= (data) => {
 
+      data.forEach((item) => {
+        const target = document.createElement('div');
+        target.classList.add('target');
+    
+    
+        target.innerHTML = `
+        <img src="${item.poster}" alt="${item.title}">
+        <div class="content">  
+        <a href="">${item.title}</a>
+        <p class="año">${item.year}</p>
+        <p class="director">${item.director}</p>
+        <p class="genero">${item.genre.join(', ')}</p>
+        <h6><span>IMDB</span><i class="bi bi-star-fill"></i>${item.rate}</h6>
+        </div>`
+        
+        const containerCards = document.getElementById('container-cards')
+        containerCards.appendChild(target);
+        
+      });
 
-console.log(tempData);
+      
+}   
+
+console.log(data);
+    
